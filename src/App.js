48ba3192b29ec0441b7bbe454205447540a7102c
ying.andrews <ying.andrews@gmail.com>
2023-11-30 20:38:29 -0500

    first cut.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useState, useEffect, useCallback } from "react";
+import JobInfo from "./components/JobInfo";
+
 function App() {
+  let [jobList, setJobList] = useState([]);
+
+  const fetchData = useCallback(
+    () => {
+      fetch('./data.json')
+        .then(response => response.json())
+        .then(data => { setJobList(data._embedded.jobs) });
+    }, []
+  );
+
+  useEffect(() => { fetchData() }, [fetchData]);
+
   return (
-    <div className="App">
-      <h2>hello</h2>
+    <>
+    <div className="flex justify-center items-center pt-12">
+      <img src="./logo.png" alt="BAE logo" />
     </div>
+
+    <div className="ml-10 mr-10 mt-10">
+      <hr />
+      <ul className="divide-y divide-gray-200">
+        {jobList
+          .map(job => (
+            <JobInfo key={job.id} job={job}/>
+          ))
+        }
+      </ul>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/JobInfo.js
@@ -0,0 +1,19 @@
+const JobInfo = ({ job }) => {
+    return (
+        <li className="px-3 py-3 flex items-start">
+            <div className="flex-grow">
+                <div className="flex items-center">
+                    <span className="flex-none font-medium text-2xl text-black">{job.title}</span>
+                    <span className="flex-grow text-right">{job.code}</span>
+                </div>
+                <div><b className="font-bold text-gray-600">Location:</b> {job.location}</div>
+                <div><b className="font-bold text-gray-600">Level:</b> {job.level}</div>
+                <div><b className="font-bold text-gray-600">Type:</b> {job.mode}</div>
+                <div><b className="font-bold text-gray-600">Starting Salary:</b> ${job.salary}</div>
+                <div className="leading-tight mt-2">{job.description}</div>
+            </div>
+        </li>
+    );
+}
+
+export default JobInfo;
